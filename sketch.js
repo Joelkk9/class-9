58e3bb4c0566d75a62e110e999745c494bd56b97
Joelkk9 <90608996+Joelkk9@users.noreply.github.com>
2021-09-13 17:11:54 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var box 
+
+function setup() {
+  createCanvas(400,400);
+  box=createSprite(200,200,20,50);
+  box.shapeColor = "blue";
+}
+
+function draw() 
+{
+  background(30);
+  drawSprites();
+}
+
+
+
+
